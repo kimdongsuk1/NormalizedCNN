a07d0d72a40928c997c35ab2ce9af8426c7fbaf8
kimdongsuk1 <gkimds@snu.ac.kr>
2020-05-21 15:08:32 +0900

    Update NCNN.cpp

--- NCNN.cpp
+++ NCNN.cpp
@@ -1,47 +1,43 @@
 #include <torch/extension.h>
-
+#include <iostream>
 #include <vector>
-
-// CUDA forward declarations
-
-std::vector<torch::Tensor> lltm_cuda_forward(
+std::vector<at::Tensor> lltm_forward(
     torch::Tensor input,
     torch::Tensor weights,
     torch::Tensor bias,
-    torch::Tensor old_h,
-    torch::Tensor old_cell);
+    torch::Tensor scale,
+    torch::Tensor kernel_size,
+    torch::Tensor dilation,
+    torch::Tensor padding,
+    torch::Tensor stride,
+    torch::Tensor filters,) {
+    
+  const auto output_h = (input.size(2) + (2*padding[0]) - ((dilation[0]*kernel_size[0]-1)+1))/stride[0] + 1
+  const auto output_w = (input.size(3) + (2*padding[1]) - ((dilation[1]*kernel_size[1]-1)+1))/stride[1] + 1
+      
+  auto X = torch::unfold(x,kernel_size,dilation=dilation,padding=padding,stride=stride).transpose(1,2);
+  
+  auto X_mean = torch::mean(X,/*dim=*/-1,keepdim=True);
+      
+  auto X_shift = X- X_mean;
+  auto X_std = torch::std(X_shift,/*dim=*/-1,keepdim=True).expand_as(X);
+  auto X_refined = X_shift / (X_std+1e-5);
+  
+  auto flatten = weights.view(weights.size(0),-1).transpose();
+  auto output = torch::addmm(bias,X_refined,flatten).view(-1,filters,output_h,output_w);
 
-std::vector<torch::Tensor> lltm_cuda_backward(
-    torch::Tensor grad_h,
-    torch::Tensor grad_cell,
-    torch::Tensor new_cell,
-    torch::Tensor input_gate,
-    torch::Tensor output_gate,
-    torch::Tensor candidate_cell,
-    torch::Tensor X,
-    torch::Tensor gate_weights,
-    torch::Tensor weights);
-
-// C++ interface
-
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
-
-std::vector<torch::Tensor> lltm_forward(
-    torch::Tensor input,
-    torch::Tensor weights,
-    torch::Tensor bias,
-    torch::Tensor old_h,
-    torch::Tensor old_cell) {
-  CHECK_INPUT(input);
-  CHECK_INPUT(weights);
-  CHECK_INPUT(bias);
-  CHECK_INPUT(old_h);
-  CHECK_INPUT(old_cell);
-
-  return lltm_cuda_forward(input, weights, bias, old_h, old_cell);
+    
+    
+    
+  return {output,
+          flatten,
+          X,
+          X_mean,
+          X_shift,
+          X_std,
+          X_refined};
 }
+
 
 std::vector<torch::Tensor> lltm_backward(
     torch::Tensor grad_h,
@@ -53,28 +49,31 @@ std::vector<torch::Tensor> lltm_backward(
     torch::Tensor X,
     torch::Tensor gate_weights,
     torch::Tensor weights) {
-  CHECK_INPUT(grad_h);
-  CHECK_INPUT(grad_cell);
-  CHECK_INPUT(input_gate);
-  CHECK_INPUT(output_gate);
-  CHECK_INPUT(candidate_cell);
-  CHECK_INPUT(X);
-  CHECK_INPUT(gate_weights);
-  CHECK_INPUT(weights);
+  auto d_output_gate = torch::tanh(new_cell) * grad_h;
+  auto d_tanh_new_cell = output_gate * grad_h;
+  auto d_new_cell = d_tanh(new_cell) * d_tanh_new_cell + grad_cell;
 
-  return lltm_cuda_backward(
-      grad_h,
-      grad_cell,
-      new_cell,
-      input_gate,
-      output_gate,
-      candidate_cell,
-      X,
-      gate_weights,
-      weights);
+  auto d_old_cell = d_new_cell;
+  auto d_candidate_cell = input_gate * d_new_cell;
+  auto d_input_gate = candidate_cell * d_new_cell;
+
+  auto gates = gate_weights.chunk(3, /*dim=*/1);
+  d_input_gate *= d_sigmoid(gates[0]);
+  d_output_gate *= d_sigmoid(gates[1]);
+  d_candidate_cell *= d_elu(gates[2]);
+
+  auto d_gates =
+      torch::cat({d_input_gate, d_output_gate, d_candidate_cell}, /*dim=*/1);
+
+  auto d_weights = d_gates.t().mm(X);
+  auto d_bias = d_gates.sum(/*dim=*/0, /*keepdim=*/true);
+
+  auto d_X = d_gates.mm(weights);
+  const auto state_size = grad_h.size(1);
+  auto d_old_h = d_X.slice(/*dim=*/1, 0, state_size);
+  auto d_input = d_X.slice(/*dim=*/1, state_size);
+
+  return {d_old_h, d_input, d_weights, d_bias, d_old_cell};
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &lltm_forward, "LLTM forward (CUDA)");
-  m.def("backward", &lltm_backward, "LLTM backward (CUDA)");
-}
+
