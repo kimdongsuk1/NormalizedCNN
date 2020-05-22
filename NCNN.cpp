7528e8541d0212be58d792f7805f14fa773921e5
kimdongsuk1 <gkimds@snu.ac.kr>
2020-05-22 14:02:34 +0900

    Update NCNN.cpp

--- NCNN.cpp
+++ NCNN.cpp
@@ -22,9 +22,12 @@ std::vector<at::Tensor> ncnn_forward(
   auto X_shift = X - X_mean;
   auto X_std = torch::std(X_shift,/*dim=*/-1,/*keepdim=*/true).expand_as(X);
   auto X_refined = X_shift / (X_std+1e-5);
+    
+  auto X_scaled = scale*X_refined
+  
   
   auto flatten = weights.view(weights.size(0),-1).transpose();
-  auto output = torch::addmm(bias,X_refined,flatten).view(-1,filters,output_h,output_w);
+  auto output = torch::addmm(bias,X_scaled,flatten).view(-1,filters,output_h,output_w);
 
     
     
@@ -41,42 +44,31 @@ std::vector<at::Tensor> ncnn_forward(
 
 std::vector<torch::Tensor> ncnn_backward(
     torch::Tensor grad_output,
-    torch::Tensor grad_weights,
-    torch::Tensor grad_bias,
     torch::Tensor kernel_size,
     torch::Tensor stride,
     torch::Tensor padding,
     torch::Tensor X,
-    torch::Tensor gate_weights,
+    torch::Tensor X_mean,
+    torch::Tensor X_std,
+    torch::Tensor X_refined,
+    torch::Tensor X_scaled,
     torch::Tensor weights) {
     
     
+  const auto numb = 1.0/kernel_size[0]*kernel_size[1]*X.size(2);
+  auto grad_output_ = grad_ouput*scale
+  auto grad_sigma = torch::sum(grad_output_ * (X-X_mean),/*axis=*/=-1,/*keepdim=*/=True)*0.5*(X_std)**(-1.5);
+  auto grad_mean = torch::sum(grad_output_*(-1./X_std.expand_as(X)),/*axis=*/=-1,/*keepdim=*/=True) + grad_sigma*numb*2.0
+      *torch::sum((X-X_mean),/*axis=*/=-1,/*keepdim=*/=True)*-1;
+  auto grad_x = grad_output_ * (1/X_std.expand_as(X))+grad_sigma*2.0*torch::sum((X-X_mean),/*axis=*/=-1,/*keepdim=*/=True)+grad_mean*numb;
+  
+  auto grad_scale = torch::sum(grad_output*X_refined,/*axis=*/=-1)
+  auto grad_bias = torch::sum(grad_output,/*axis=*/=-1)
+                               
+
+  auto grad_weights = X_scaled.t().mm(grad_output);
     
-  auto d_output_gate = torch::tanh(new_cell) * grad_h;
-  auto d_tanh_new_cell = output_gate * grad_h;
-  auto d_new_cell = d_tanh(new_cell) * d_tanh_new_cell + grad_cell;
-
-  auto d_old_cell = d_new_cell;
-  auto d_candidate_cell = input_gate * d_new_cell;
-  auto d_input_gate = candidate_cell * d_new_cell;
-
-  auto gates = gate_weights.chunk(3, /*dim=*/1);
-  d_input_gate *= d_sigmoid(gates[0]);
-  d_output_gate *= d_sigmoid(gates[1]);
-  d_candidate_cell *= d_elu(gates[2]);
-
-  auto d_gates =
-      torch::cat({d_input_gate, d_output_gate, d_candidate_cell}, /*dim=*/1);
-
-  auto d_weights = d_gates.t().mm(X);
-  auto d_bias = d_gates.sum(/*dim=*/0, /*keepdim=*/true);
-
-  auto d_X = d_gates.mm(weights);
-  const auto state_size = grad_h.size(1);
-  auto d_old_h = d_X.slice(/*dim=*/1, 0, state_size);
-  auto d_input = d_X.slice(/*dim=*/1, state_size);
-
-  return {d_old_h, d_input, d_weights, d_bias, d_old_cell};
+  return {grad_weights, grad_bias, grad_scale, grad_x, };
 }
 
 
